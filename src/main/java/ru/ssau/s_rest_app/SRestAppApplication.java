3cec80ccf74b4bd84123e8a2b6fb802c547877ce
Dasha Shevlya <shevliakova.d@gmail.com>
2026-02-21 18:56:37 +0400

    update

---
+++ src/main/java/ru/ssau/s_rest_app/SRestAppApplication.java
@@ -0,0 +1,13 @@
+package ru.ssau.s_rest_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SRestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SRestAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/ssau/s_rest_app/SRestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ssau.s_rest_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SRestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
